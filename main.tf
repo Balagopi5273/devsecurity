47cb824eda34f8dda6438195cf87939fef114dbb
Balagopi <baluchandu411@gmail.com>
2024-06-19 11:59:49 +0530

    updated code

--- main.tf
+++ main.tf
@@ -117,7 +117,7 @@ EOF
 }
 
 resource "aws_instance" "web" {
-  ami             = "ami-08a0d1e16fc3f61ea"
+  ami             = "ami-0c55b159cbfafe1f0"
   instance_type   = "t2.medium" 
   key_name        = var.key_name
   iam_instance_profile = "${aws_iam_instance_profile.test_profile.name}"
